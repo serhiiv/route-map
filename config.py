f006803e2f1a812f152c4499cea3467619d02644
Serhii Vykaliuk <serhii.v@gmail.com>
2018-12-24 13:12:02 +0200

    added database interface

--- config.py
+++ config.py
@@ -2,7 +2,13 @@ import os
 from flask.cli import load_dotenv
 
 load_dotenv()
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECRET_KEY for repository'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+        'sqlite:///' + os.path.join(basedir, 'repository.db.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
